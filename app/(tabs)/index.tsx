4971b929bdaf927c6a2a899d599f828c7fe6f32f
Ramdan <dadanisme@upi.edu>
2026-02-23 06:20:48 +0700

    dummy logout button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+
+import { useAuth } from '@/store';
 
 export default function HomeScreen() {
+  const { signOut } = useAuth();
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello World - Home</Text>
+      <Button title="Logout" onPress={signOut} />
     </View>
   );
 }
