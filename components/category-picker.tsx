7b06ad0dc3778ae308912302234150c88bfbcc35
Ramdan <dadanisme@upi.edu>
2026-02-23 23:28:13 +0700

    centered

--- components/category-picker.tsx
+++ components/category-picker.tsx
@@ -86,6 +86,7 @@ const styles = StyleSheet.create({
     fontSize: design.fontSize.lg,
     fontWeight: '800',
     color: colors.black,
+    textAlign: 'center',
     marginBottom: design.spacing.md,
   },
   row: {
